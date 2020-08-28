66d974af34c4ccd76397f490a58c87848e992f6f
Saulo Santiago <saulodasilvasantiago@gmail.com>
2020-08-27 23:56:45 -0300

    Add conf to sidekiq

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -4,5 +4,6 @@ require 'datadog/statsd'
 if Rails.env.production?
   Datadog.configure do |c|
     c.use :rails, service_name: 'dokkuexample'
+    c.use :sidekiq, service_name: 'dokkuexample', client_service_name: 'dokkuexample-sidekiq', tag_args: true
   end
 end
